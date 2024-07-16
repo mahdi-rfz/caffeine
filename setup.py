b381937d7ec6b944e8abdc9d7ad4155f6266b1b1
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-16 09:24:19 +0330

    creat setup base

--- setup.py
+++ setup.py
@@ -1,2 +1,18 @@
 import os
 import colorama
+
+os.system("pip install pyautogui")
+os.system("pip install pyinstaller")
+
+os.system("pyinstaller --onefile caffeine.py")
+os.chdir("dist")
+os.system("cp caffeine /bin")
+
+
+os.chdir("/etc")
+os.mkdir("caffeine")
+os.chdir("caffeine")
+
+os.system("cp caffeine_config.py /etc/caffeine")
+
+
