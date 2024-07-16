52e19c4266531b9466a49c55090a7f2ffff9b327
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-16 07:47:39 +0330

    base

--- caffeine.py
+++ caffeine.py
@@ -2,5 +2,26 @@ import os
 import time
 import datetime 
 import pyautogui
+import sys
 
-x = pyautogui.moveTo(1)
+sys.path.insert(0 , "/etc/caffeine")
+import caffeine_config
+
+type_of_movement = caffeine_config.type_of_movement
+mouse_coordinates = caffeine_config.mouse_coordinates
+custom_key = caffeine_config.custom_key
+delay_time = caffeine_config.delay_time
+
+
+def mouse_mv():
+    pass
+
+def keyboard_mv():
+    pass
+
+
+
+if type_of_movement == (1) :
+    mouse_mv(mouse_coordinates , delay_time)
+else :
+    keyboard_mv(custom_key , delay_time)
