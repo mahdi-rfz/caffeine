6f81c2300471fcd2094d234846805e8a52a9efd4
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-16 09:24:33 +0330

    fix config file

---
+++ caffeine_config.py
@@ -0,0 +1,17 @@
+type_of_movement = (1)
+# 1 for mouse move , 2 for press key on keyboard
+
+delay_time = (5) #secend
+
+mouse_coordinates = [100 , 100]
+#                     x ,  y
+
+custom_key = ("c")
+# for click enter use ----> ["enter"]
+# for click alt   use ----> ["alt"]
+# for click shift use ----> ["shift"]
+# for click up    use ----> ["up"]
+# for click down use ----> ["down"]
+# for click right use ----> ["right"]
+# for click left use ----> ["left"]
+# for click other key type key name (for example ("h"))
