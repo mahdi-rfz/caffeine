df5f4a0f38da7e7fcf378dc3c32b171d82385551
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-16 09:23:35 +0330

    fix bugs

--- caffeine.py
+++ caffeine.py
@@ -4,6 +4,20 @@ import datetime
 import pyautogui
 import sys
 
+
+os.system("clear")
+
+def typography ():
+    return """ ▄▀▀ ▄▀▄ █▀ █▀ ██▀ █ █▄ █ ██▀
+ ▀▄▄ █▀█ █▀ █▀ █▄▄ █ █ ▀█ █▄▄
+"""
+
+def ctime() :
+    cu = datetime.datetime.now()
+    time = cu.strftime('%H:%M %p')
+    return time
+
+
 sys.path.insert(0 , "/etc/caffeine")
 import caffeine_config
 
@@ -13,15 +27,36 @@ custom_key = caffeine_config.custom_key
 delay_time = caffeine_config.delay_time
 
 
-def mouse_mv():
-    pass
 
-def keyboard_mv():
-    pass
+def mouse_mv(mouse_crd , dl_time):
+    pyautogui.moveTo(mouse_crd[0] , mouse_crd[1])
+    time.sleep(dl_time)
+    
+    mouse_mv(mouse_coordinates , delay_time)
+
+
+
+def keyboard_mv(ckey , dl_time):
+    pyautogui.typewrite(ckey)
+    time.sleep(dl_time)
+    
+    keyboard_mv(custom_key , delay_time)
 
 
 
 if type_of_movement == (1) :
+    print(typography())
+    print(f"Time : {ctime()} , Type of movement : move mouse")
+    print(f"Moveto : {mouse_coordinates}")
+    print(f"Press ctrl + c for stop and exit")
+    print("_______________________________")
     mouse_mv(mouse_coordinates , delay_time)
+    
 else :
+    print(typography())
+    print(f"Time : {ctime()} , Type of movement : press key")
+    print(f"Press key : {custom_key}")
+    print(f"Press ctrl + c for stop and exit")
+    print("_______________________________")
     keyboard_mv(custom_key , delay_time)
+    
