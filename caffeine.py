94104c6c5931112abd9e86068890ace773a3eaf9
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-07-31 04:41:34 +0330

    complet coaffeine file

--- caffeine.py
+++ caffeine.py
@@ -2,61 +2,48 @@ import os
 import time
 import datetime 
 import pyautogui
+from colorama import Fore
 import sys
 
 
 os.system("clear")
 
 def typography ():
-    return """ ▄▀▀ ▄▀▄ █▀ █▀ ██▀ █ █▄ █ ██▀
+    return Fore.LIGHTBLACK_EX + """ ▄▀▀ ▄▀▄ █▀ █▀ ██▀ █ █▄ █ ██▀
  ▀▄▄ █▀█ █▀ █▀ █▄▄ █ █ ▀█ █▄▄
-"""
+""" + Fore.WHITE
 
 def ctime() :
     cu = datetime.datetime.now()
     time = cu.strftime('%H:%M %p')
     return time
 
-
 sys.path.insert(0 , "/etc/caffeine")
-import caffeine_config
-
-type_of_movement = caffeine_config.type_of_movement
-mouse_coordinates = caffeine_config.mouse_coordinates
-custom_key = caffeine_config.custom_key
-delay_time = caffeine_config.delay_time
+import caffeine_config as cf_conf
 
 
+def move_mouse():
+    while True:
+        mover = pyautogui.moveTo(cf_conf.mouse_coordinates[0] , cf_conf.mouse_coordinates[1])
+        time.sleep(cf_conf.delay_time)
 
-def mouse_mv(mouse_crd , dl_time):
-    pyautogui.moveTo(mouse_crd[0] , mouse_crd[1])
-    time.sleep(dl_time)
-    
-    mouse_mv(mouse_coordinates , delay_time)
-
-
-
-def keyboard_mv(ckey , dl_time):
-    pyautogui.typewrite(ckey)
-    time.sleep(dl_time)
-    
-    keyboard_mv(custom_key , delay_time)
+def press_key():
+    while True:
+        press = pyautogui.press(cf_conf.custom_key)
+        time.sleep(cf_conf.delay_time)
 
 
-
-if type_of_movement == (1) :
+if cf_conf.type_of_movement == ("MOUSE") :
     print(typography())
-    print(f"Time : {ctime()} , Type of movement : move mouse")
-    print(f"Moveto : {mouse_coordinates}")
-    print(f"Press ctrl + c for stop and exit")
-    print("_______________________________")
-    mouse_mv(mouse_coordinates , delay_time)
-    
+    print(f"Type of movement : Move mouse , Start Time : {ctime()}")
+    print(f"The Length : {cf_conf.mouse_coordinates[0]} , The Width : {cf_conf.mouse_coordinates[1]}")
+    print(Fore.LIGHTBLACK_EX + "========================================================" + Fore.WHITE)
+    move_mouse()
+
 else :
     print(typography())
-    print(f"Time : {ctime()} , Type of movement : press key")
-    print(f"Press key : {custom_key}")
-    print(f"Press ctrl + c for stop and exit")
-    print("_______________________________")
-    keyboard_mv(custom_key , delay_time)
+    print(f"Type of movement : Press key , Start Time : {ctime()}")
+    print(f"Custom Keyboard Key : {cf_conf.custom_key}")
+    print(Fore.LIGHTBLACK_EX + "========================================================" + Fore.WHITE)
+    press_key()
     
